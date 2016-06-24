980b047c93d434c465b94bc6558d022b2b5683d2
Umair Hussain <umair.hussain@hootsuite.com>
2016-06-24 15:59:40 -0700

    Auth lib now uses new getter

--- phplib/Auth.php
+++ phplib/Auth.php
@@ -9,12 +9,6 @@ class MorgueAuth {
      * @returns auth data as a dictionary
      */
     static function get_auth_data() {
-        if (isset($_SESSION["username"])) {
-            $admin_data =  array("username" => $_SESSION["username"]);
-        } else {
-            $admin_data =  array("username" => "Not Signed In");
-            // header("Location:index.php");
-        }
-        return $admin_data;
+        return array("username" => Configuration::get_current_username());
     }
 }
