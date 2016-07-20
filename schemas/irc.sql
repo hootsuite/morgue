f1ff61d7a070505d0ab0d6ad0d049f0f19e2a9ac
Umair Hussain <umair.hussain@hootsuite.com>
2016-07-20 13:40:30 -0700

    stops deleting tables if they exist

--- schemas/irc.sql
+++ schemas/irc.sql
@@ -2,10 +2,9 @@
 -- Table structure for irc feature
 --
 
-DROP TABLE IF EXISTS `irc`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `irc` (
+CREATE TABLE IF NOT EXISTS `irc` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `postmortem_id` bigint(20) NOT NULL,
   `channel` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
